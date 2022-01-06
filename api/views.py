fb148bc5e75c588a69e543b3e130e8a03cd95f11
손봉우 <bongudev@gmail.com>
2022-01-06 13:34:40 +0000

    simple list

--- api/views.py
+++ api/views.py
@@ -1,6 +1,9 @@
+from django.shortcuts import render
+
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
+from rest_framework.renderers import TemplateHTMLRenderer
 
 from .models import Artist
 from .serializers import ArtistSerializer
@@ -9,13 +12,24 @@ from .serializers import ArtistSerializer
 
 class ArtistAPI (APIView):
     
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'index.html'
+    
     def get (self, req):
+        queryset = Artist.objects.all()
+        # model = Artist.objects.all()
         
-        model = Artist.objects.all()
+        # serializer = ArtistSerializer(model, many=True)
         
-        serializer = ArtistSerializer(model, many=True)
+        # return Response(serializer.data)def index(request):
+
+        # context = {
+        #     'artist': model
+        # }
         
-        return Response(serializer.data)
+        return Response( {'artist': queryset} )
+        # return render(req, 'index.html')
+        
     
     def post (self, req):
         
